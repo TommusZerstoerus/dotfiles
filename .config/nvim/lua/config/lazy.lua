ec916792f81177c41156593b294e63f0603ba6e3
tbecke <tbecke@progeek.de>
2025-02-26 09:59:23 +0100

    Add: .zshrc

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -21,19 +21,19 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
     {
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({
-      view_options = {
-        show_hidden = true
-      }
-      })
-    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
-  end
-}
+      "stevearc/oil.nvim",
+      opts = {},
+      -- Optional dependencies
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      config = function()
+        require("oil").setup({
+          view_options = {
+            show_hidden = true,
+          },
+        })
+        vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+      end,
+    },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
